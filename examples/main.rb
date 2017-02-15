d4ed8c6f8de95e53056ec2af31caecb58566dcf8
Stephanie Tsai <stephanie.tsai@gusto.com>
2017-02-14 18:00:20 -0800

    small changes to prawn

--- examples/main.rb
+++ examples/main.rb
@@ -8,13 +8,13 @@ require 'prawn-fillform'
 
 data = {}
 data[:page_1] = {}
-data[:page_1][:firstname] = { :value => "Max" }
-data[:page_1][:photo] = { :value => "../data/test.jpg", :options => { fill: true } }
+data[:page_1]['firstname'] = { :value => "Max" }
+data[:page_1]['photo'] = { :value => "../data/test.jpg", :options => { fill: true } }
 # Page number optional, substitute lastname var in all pages
-data[:lastname] = { :value => "Mustermann" }
-data[:checked] = { :value => "true" }
-data[:comb] = { :value => "123456", :options => { :font_size => 20 } }
-data[:comb_too_small] = { :value => "123456789" }
+data['lastname'] = { :value => "Mustermann" }
+data['checked'] = { :value => "true" }
+data['comb'] = { :value => "123456", :options => { :font_size => 20 } }
+data['comb_too_small'] = { :value => "123456789" }
 
 
 Prawn::Document.generate "../data/output.pdf", :template => "../data/template.pdf"  do |pdf|
